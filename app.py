7469ab8870c8fc1ec0a8c73877d4853986eca4d4
pprain1999 <pjungood@naver.com>
2025-04-02 17:03:18 +0900

    게임 시작 + 게임 진행

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ selected_round = st.selectbox("플레이할 라운드를 선택하세요:", roun
 
 if st.button("게임 시작"):
     st.session_state.round = selected_round
-    st.session_state.winner = {
-        "name": "떡볶이",
-        "image_url": "./img/떡볶이.png"
-    }  # 임시 결과
-    st.switch_page("pages/game_result.py")
+    st.session_state.winner = {}
+
+    st.switch_page("pages/main.py")
+
+
 
     
